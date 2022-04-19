f8f5a522e2c5fc51aca80a45ae80eecb90ab0a65
shellieD <shellie.downie13@gmail.com>
2022-04-19 19:47:48 +0000

    Deployment Commit

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+from django.contrib import messages
 
 
 class PostList(generic.ListView):
@@ -49,6 +50,7 @@ class PostDetail(View):
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
+            messages.success(request, "Your comment has been submitted and is awaiting approval.")
             comment.save()
         else:
             comment_form = CommentForm()
